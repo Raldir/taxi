f5341026fb0987a749505b17c54e8847e26313eb
Alexander Ossa <6ossa@ltcpu1.informatik.uni-hamburg.de>
2018-10-16 22:28:10 +0200

    Added domains to run.sh

--- run.sh
+++ run.sh
@@ -19,19 +19,21 @@ FILE_CLEANED_OUT=${FILE_PRUNED_OUT}-cleaned.csv
 
 FILE_EVAL_TOOL_RESULT=${FILE_CLEANED_OUT}-evalresul.txt
 
-
-
-
-DOMAIN=$EVAL_ROOT
 if [ -n "$2" ]; then
 	CYCLE_REMOVING_METHOD=$2
 fi
+
+
+if [ -n "$3" ]; then
+        EVAL_ROOT=$3
+fi
+
 
 echo Reading input: $1
 echo Reading file: $FILE_INPUT
 echo Output directory: $OUTPUT_DIR
 echo Cycle removing method: $CYCLE_REMOVING_METHOD
-echo Domain: $DOMAIN
+echo Domain: $EVAL_ROOT
 
 echo
 
@@ -47,20 +49,16 @@ echo "Cycle removing finished. Written to: $OUTPUT_DIR/$FILE_PRUNED_OUT"
 echo
 
 echo "======================================================================================================================"
-echo "Cleaning: python $CLEANING_TOOL $OUTPUT_DIR/$FILE_PRUNED_OUT $OUTPUT_DIR/$FILE_CLEANED_OUT $DOMAIN"
-python3 $CLEANING_TOOL $OUTPUT_DIR/$FILE_PRUNED_OUT $OUTPUT_DIR/$FILE_CLEANED_OUT $DOMAIN
+echo "Cleaning: python $CLEANING_TOOL $OUTPUT_DIR/$FILE_PRUNED_OUT $OUTPUT_DIR/$FILE_CLEANED_OUT $EVAL_ROOT"
+python3 $CLEANING_TOOL $OUTPUT_DIR/$FILE_PRUNED_OUT $OUTPUT_DIR/$FILE_CLEANED_OUT $EVAL_ROOT
 echo "Finished cleaning. Write output to: $OUTPUT_DIR/$FILE_CLEANED_OUT"
 echo
 
 
 echo "======================================================================================================================"
-
 echo "Running eval-tool: java $EVAL_JVM -jar $EVAL_TOOL $OUTPUT_DIR/$FILE_CLEANED_OUT $EVAL_GOLD_STANDARD $EVAL_ROOT $OUTPUT_DIR/$FILE_EVAL_TOOL_RESULT"
 java $EVAL_JVM -jar $EVAL_TOOL $OUTPUT_DIR/$FILE_CLEANED_OUT $EVAL_GOLD_STANDARD $EVAL_ROOT $OUTPUT_DIR/$FILE_EVAL_TOOL_RESULT 2> $OUTPUT_DIR/eval.out
-
 echo "Result of eval-tool written to: $OUTPUT_DIR/$FILE_EVAL_TOOL_RESULT"
-
-
 echo "======================================================================================================================"
 
 L_GOLD="$(wc -l $EVAL_GOLD_STANDARD | grep -o -E '^[0-9]+').0"
@@ -82,6 +80,6 @@ echo "F&M:       $F_M"
 echo
 
 echo "Copy to https://docs.google.com/spreadsheets/d/1cTUfm97m3vhnOOzYvbqzhJhFQSyWySszLcA6PYf8wFY/edit?usp=sharing"
-echo -e "$(date +%F)\t$(whoami)\ttaxi\t$1\t$DOMAIN\t$CYCLE_REMOVING_METHOD\t$CYCLES_REMOVED\t$RECALL\t$PRECISION\t$F1\t$F_M"
+echo -e "$(date +%F)\t$(whoami)\ttaxi\t$1\t$EVAL_ROOT\t$CYCLE_REMOVING_METHOD\t$CYCLES_REMOVED\t$RECALL\t$PRECISION\t$F1\t$F_M"
 echo
 echo "Script finished."
